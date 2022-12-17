2bb9f0f2067c6d52001b70ed5b1a7000d9ae3973
mitja-hofer <mitja.hofer@celtra.com>
2022-12-17 15:28:16 +0100

    search recipes and ingredients on ui

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -18,11 +18,61 @@ function getMyCookies() {
     })
 }
 
-function searchRecipes() {
+function searchRecipesByName() {
     const filter = document.getElementById("recipe-search").value
     const recipeList = document.getElementById("recipe-list-search")
+    recipeList.innerHTML = ""
+    fetch(`../../recipes/name/${filter}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + sessionStorage.getItem('token')
+        }
+    }).then(res => {
+        if (!res.ok) {
+            if (res.status == 404) {
+                recipeList.innerHTML = "no recipes found"
+                return
+            } else {
+                throw new Error("response was not ok")
+            }
+        }
+        res.json().then(data => {
+            recipeList.innerHTML = generateRecipeList(data)
+        })
+    })
+}
+
+function searchRecipesByIngredient() {
+    const filter = document.getElementById("recipe-search").value
+    const recipeList = document.getElementById("recipe-list-search")
+    recipeList.innerHTML = ""
+    fetch(`../../recipes/ingredient/${filter}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + sessionStorage.getItem('token')
+        }
+    }).then(res => {
+        if (!res.ok) {
+            if (res.status == 404) {
+                recipeList.innerHTML = "no recipes found"
+                return
+            } else {
+                throw new Error("response was not ok")
+            }
+        }
+        res.json().then(data => {
+            recipeList.innerHTML = generateRecipeList(data)
+        })
+    })
+}
+
+function searchIngredients() {
+    const filter = document.getElementById("ingredient-search").value
+    const ingredientList = document.getElementById("ingredient-list-search")
     if (filter.length >= 2) {
-        fetch(`../../recipes/name-like/${filter}`, {
+        fetch(`../../ingredients/name-like/${filter}`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -31,24 +81,27 @@ function searchRecipes() {
         }).then(res => {
             if (!res.ok) {
                 if (res.status == 404) {
-                    recipeList.innerHTML = "no recipes found"
+                    ingredientList.innerHTML = "no ingredients found"
                     return
                 } else {
-                    recipeList.innerHTML = ""
+                    ingredientList.innerHTML = ""
                     throw new Error("response was not ok")
                 }
             }
             res.json().then(data => {
-                console.log(data)
-                recipeList.innerHTML = generateRecipeList(data)
-                console.log(recipeList.innerHTML)
+                ingredientList.innerHTML = generateIngredientList(data)
             })
         })
+    } else {
+        ingredientList.innerHTML = ""
     }
-
 }
 
+
 function generateRecipeList(data) {
+    if (data == null) {
+        return "No recipes found."
+    }
     return `
     <ul class="recipe-list">
                     ${data.map(recipe =>
@@ -59,4 +112,19 @@ function generateRecipeList(data) {
                      </li>`
     ).join('')}
     </ul>`
+}
+
+function generateIngredientList(data) {
+    if (data == null) {
+        return "No ingredients found."
+    }
+    return `
+    <ul class="ingredient-list">
+                    ${data.map(ingredient =>
+        `<li class="ingredient-list-element" id="ingredient-list-id-${ingredient.id}">
+                        ${ingredient.name}
+                     </li>`
+    ).join('')}
+    </ul>`
+
 }
--- routes/routes.go
+++ routes/routes.go
@@ -21,6 +21,6 @@ func PrivateRoutes(g *gin.RouterGroup) {
 	g.GET("/recipes/name-like/:name", controllers.GetRecipesByPartialName())
 	g.GET("/recipes/user-id/:userId", controllers.GetRecipesByUserId())
 	g.GET("/ingredients/recipe-id/:recipeId", controllers.GetIngredientsByRecipeId())
-	g.GET("/ingredients/name/:name", controllers.GetIngredientsByPartialName())
+	g.GET("/ingredients/name-like/:name", controllers.GetIngredientsByPartialName())
 	g.POST("/ingredients", controllers.PostIngredient())
 }
